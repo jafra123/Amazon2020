7721feae551f63e2c4530c6e32cda1297408b079
jafra123 <36800831+jafra123@users.noreply.github.com>
2020-03-06 15:31:17 -0600

    Update Anotaion2.java

--- src/test/java/Amazon/Anotaion2.java
+++ src/test/java/Amazon/Anotaion2.java
@@ -6,7 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class Anotaion2 {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\b\\eclipse-workspace\\ARpanaTestNG\\drv\\chromedriver.exe");
+		
 	WebDriver driver;
+	driver = new ChromeDriver();
+	
 	@Test
 	public void test1() {
 		System.out.println("test1");
