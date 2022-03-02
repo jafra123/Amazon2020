beaac005f7fda368835fdc5ec2f4c776fbcd49a2
jafra123 <manmohanjafra@gmail.com>
2022-03-01 22:01:20 -0600

    added

---
+++ src/test/java/Amazon/BranchTest.java
@@ -0,0 +1,13 @@
+package Amazon;
+
+public class BranchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello this is me ");
+	
+	}
+
+}
